f56c980f8b165c3b3fefe5ceb1267f8caa41adf6
watanabe.everystudio <watanabe.everystudio@gmail.com>
2016-12-20 22:23:25 +0900

    ipad用対応

---
+++ script/AdjustUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdjustUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		if( 2000 < Screen.width)
+		{
+			gameObject.transform.localScale *= 0.5f;
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

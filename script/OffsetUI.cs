df51b5ea063b058dc34e1d2ea89f2643e99df285
watanabe.everystudio <watanabe.everystudio@gmail.com>
2016-12-21 21:54:24 +0900

    無理やりオフセットさせる処理が必要っぽい

---
+++ script/OffsetUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OffsetUI : MonoBehaviour {
+
+	public float offset_x;
+	public float offset_y;
+
+	// Use this for initialization
+	void Start () {
+		//if (2000 < Screen.width)
+		{
+			Vector3 pos = gameObject.transform.localPosition;
+			pos = new Vector3(pos.x + offset_x, pos.y + offset_y, pos.z);
+			gameObject.transform.localPosition = pos;
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
